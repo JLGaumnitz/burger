69b4b0d03e43729e83d04c8ccfa1724330503d4a
JLG <mjgaumnitz@kc.rr.com>
2019-12-13 16:56:54 -0600

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hickory Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mega Burger", FALSE);
